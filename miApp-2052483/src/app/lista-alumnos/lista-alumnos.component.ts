acf028f4f4766765bbaf66ef82a49f871cad41fc
Artu-GR <arturo.drago@gmail.com>
2024-04-02 18:38:46 -0600

    Practica8: Action Sheet y Sliding Options

--- miApp-2052483/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2052483/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, numberAttribute } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAlumnosComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit() {}
 
+  async presentActionSheet(name: string) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+          handler: () => {
+            this.delFav(name);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
   alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+
+  favoritos : any = [];
+
+  addFav(nombre: string): void{
+
+    this.favoritos.push(nombre);
+    alert(nombre + " se ha agregado correctamente a favoritos");
+
+  }
+
+  delFav(nombre: string): void{
+
+    var index : number = this.favoritos.indexOf(nombre, 0);
+    delete(this.favoritos[index]);
+
+    alert(nombre + " ha sido eliminado de favoritos");
+  }
+
 }
